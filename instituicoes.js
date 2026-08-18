9f4a43336b1f49372b30bb5693062b03ccb076cc
jeder2101 <60998512+jeder2101@users.noreply.github.com>
2026-08-18 10:05:06 -0300

    Create instituicoes.js

---
+++ instituicoes.js
@@ -0,0 +1,19 @@
+const dadosInstituicoes = {
+    "associacao-curucaca": {
+        nome: "Associação Curucaca",
+        missao: "Escreva a missão aqui...",
+        acoes: "Escreva as ações aqui...",
+        fotos: [
+            "assets/logos/associacao-curucaca/foto1.jpg",
+            "assets/logos/associacao-curucaca/foto2.jpg"
+        ]
+    },
+    "escola-karugwa": {
+        nome: "E.E.I. Aldeia Karugwá Pei",
+        missao: "Missão da escola...",
+        acoes: "Ações da escola...",
+        fotos: [
+            "assets/logos/escola-karugwa/foto1.jpg"
+        ]
+    }
+};
