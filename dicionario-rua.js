441ebc93001da84ca09ec932b7bb1076e6d7d827
jeder2101 <60998512+jeder2101@users.noreply.github.com>
2026-08-28 16:04:27 -0300

    Update dicionario-rua.js

--- dicionario-rua.js
+++ dicionario-rua.js
@@ -6,6 +6,15 @@ const DICIONARIO_RUA = [
         sentido_de: "Termo originalmente descritivo da fauna, associado pejorativamente a pessoas de pele preta (kambá) sob influência do preconceito externo.",
         falante: "Anônimo",
         exemplos: ["Kambá oiko ka'i rami."],
-        traducao: ["O preto anda como macaco."]
+        traducao: ["O preto vive como macaco."]
+    },
+    {
+        palavra: "mandopá",
+        tipo: "linguagem de rua",
+        significado: "Expressão pejorativa de ofensa equivalente a txewori.",
+        sentido_de: "Termo de forte conotação depreciativa usado na oralidade informal de campo com intenção de maldade ou xingamento direto.",
+        falante: "Anônimo",
+        exemplos: ["Ha'ã, mondopá ndejuro."],
+        traducao: ["Vou mandar o pa# na sua boca."]
     }
 ];
