2abd8638d9ea8ef718ad2c540ebd933762459f5e
jeder2101 <60998512+jeder2101@users.noreply.github.com>
2026-08-28 12:47:04 -0300

    Update dicionario-rua.js

--- dicionario-rua.js
+++ dicionario-rua.js
@@ -1,1 +1,11 @@
-
+const DICIONARIO_RUA = [
+    {
+        palavra: "ka'i",
+        tipo: "linguagem de rua",
+        significado: "Macaco.",
+        sentido_de: "Termo originalmente descritivo da fauna, associado pejorativamente a pessoas de pele preta (kambá) sob influência do preconceito externo.",
+        falante: "Anônimo",
+        exemplos: ["Kambá oiko ka'i rami."],
+        traducao: ["O preto anda como macaco."]
+    }
+];
